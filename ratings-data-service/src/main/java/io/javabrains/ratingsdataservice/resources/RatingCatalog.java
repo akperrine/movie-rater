e3f3d3d478e1ea096eebef5b8b06579b0a785273
Austin Perrine <akperrine@gmail.com>
2023-04-11 16:55:30 -0700

    first commit

---
+++ ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingCatalog.java
@@ -0,0 +1,30 @@
+package io.javabrains.ratingsdataservice.resources;
+
+import io.javabrains.ratingsdataservice.models.Rating;
+import io.javabrains.ratingsdataservice.models.UserRating;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ratings")
+public class RatingCatalog {
+    @GetMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @GetMapping("users/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+            new Rating("1234", 6), new Rating("5678", 3)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
+    }
+}
